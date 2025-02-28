bfc259d52682f2296f3076632865d72753b7e066
dhalini <dhalini99@gmail.com>
2025-02-28 14:30:20 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import routing components
+import App from './App';
+import TermsAndConditions from './components/TermsAndConditions'; // Import the TermsAndConditions component
+import './index.css'; // Ensure your global styles are imported
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} /> {/* Default route for the homepage */}
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} /> {/* Route for the Terms and Conditions page */}
+      </Routes>
+    </Router>
   </React.StrictMode>
-)
+);
